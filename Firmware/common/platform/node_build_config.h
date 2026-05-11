ead0e4f0de74c8717c42c1ca3f0bb5379b09e2d5
HMRN_SiO2 <hidem.1129@gmail.com>
2026-05-11 20:29:10 +0900

    Update node_build_config.h

--- Firmware/common/platform/node_build_config.h
+++ Firmware/common/platform/node_build_config.h
@@ -3,10 +3,16 @@
 
 #include "node_profile.h"
 
+/*
+Uncomment the appropriate lines to match the firmware you are writing.
+For example, if you want to write a servo node, uncomment the line:
+//#define ACTIVE_NODE_PROFILE NODE_PROFILE_SERVO_BASIC
+*/
+
 //#define ACTIVE_NODE_PROFILE NODE_PROFILE_DUMMY
 //#define ACTIVE_NODE_PROFILE NODE_PROFILE_MOTOR_BASIC
 //#define ACTIVE_NODE_PROFILE NODE_PROFILE_SENSOR_BASIC
-#define ACTIVE_NODE_PROFILE NODE_PROFILE_SERVO_BASIC
+//#define ACTIVE_NODE_PROFILE NODE_PROFILE_SERVO_BASIC
 
 
 #endif
