632d230d2795e39aa5d0a1b9a3ed673428d79b8e
HMRN_SiO2 <hidem.1129@gmail.com>
2026-05-14 22:52:01 +0900

    Update run_i2c_debugger.sh

--- Software/I2C_Debugger/run_i2c_debugger.sh
+++ Software/I2C_Debugger/run_i2c_debugger.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cd /home/hmrn-sio2/i2c_debugger || exit 1
-source /home/hmrn-sio2/i2c_debugger/.venv/bin/activate
+# Update this path to your local I2C_Debugger directory.
+APP_DIR="/path/to/Software/I2C_Debugger"
+
+cd "$APP_DIR" || exit 1
+source ".venv/bin/activate"
 
 python3 i_2_c_debugger_prototype.py
